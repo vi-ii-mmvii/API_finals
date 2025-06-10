492dc0152593d16852a554bce2a7b5f522e4b9f1
vi-ii-mmvii <nur.adina.muratkyzy@gmail.com>
2025-06-11 02:19:42 +0500

    we dont need them

--- API_on_REACT/src/Search.jsx
+++ API_on_REACT/src/Search.jsx
@@ -6,9 +6,6 @@ export default function Search({ onSearch }) {
           placeholder="Прошу, к вашим услугам, поисковичок"
           onChange={(e) => onSearch(e.target.value)}
         />
-<button id="random" onClick={handleRandomClick}>
-      Random
-    </button>
       </div>
     );
   }
