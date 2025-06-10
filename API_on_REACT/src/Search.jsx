7d737b72e734dadc6ea86d35cd65c1e7f0620303
vi-ii-mmvii <nur.adina.muratkyzy@gmail.com>
2025-06-11 02:17:17 +0500

    css

--- API_on_REACT/src/Search.jsx
+++ API_on_REACT/src/Search.jsx
@@ -1,11 +1,14 @@
 export default function Search({ onSearch }) {
     return (
       <div>
-        <input
-          type="text"
+        <input id="search"
+        input="text"
           placeholder="Прошу, к вашим услугам, поисковичок"
           onChange={(e) => onSearch(e.target.value)}
         />
+<button id="random" onClick={handleRandomClick}>
+      Random
+    </button>
       </div>
     );
   }
